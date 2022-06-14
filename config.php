4ed76888c60eca556ad309995deec32d6963e903
k-ardliyan <k.ardliyan@gmail.com>
2022-06-14 15:49:15 +0700

    ada bug dan belum di fix

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+// Create Connection to Database MYSQL
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "ccm";
+$mysqli = new mysqli($host, $user, $pass, $db);
+if($mysqli->connect_error) {
+    exit('Could not connect');
+  }
+?>
